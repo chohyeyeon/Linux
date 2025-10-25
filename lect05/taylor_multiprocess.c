8d345755958236a6601403540ed9fecce1bdb12c
hyeyeon25 <chohyeyeon1125@gmail.com>
2025-10-25 14:39:45 +0900

    251022

--- lect05/taylor_multiprocess.c
+++ lect05/taylor_multiprocess.c
@@ -3,64 +3,69 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <math.h>
+#include <string.h>
 
+#define _USE_MATH_DEFINES
+#define MAXLINE 100
 #define N 4
-#define TERMS 3
 
-double sinx_taylor(double x, int terms) {
-    double sum = x;
-    double power = x * x * x;
-    double fact = 6.0;
-    int s = -1;
+void sinx_taylor(int num_elements, int terms, double* x, double* result) {
+    int child_id, pid;
+    int length;
+    char message[MAXLINE], line[MAXLINE];
+    int fd[2 * N];
 
-    for(int k = 1; k <= terms; k++) {
-        sum += s * (power / fact);
-        power *= x * x;
-        fact *= (2.0*k + 2.0) * (2.0*k + 3.0);
-        s = -s;
+    for (int i = 0; i < num_elements; i++) {
+        child_id = i;
+        pipe(fd + 2 * i);
+        pid = fork();
+        if (pid == 0) break;
     }
-    return sum;
+
+    if (pid == 0) {
+        int i = child_id;
+        close(fd[2 * i]);
+        double value = x[i];
+        double numer = x[i] * x[i] * x[i];
+        double denom = 6.0;
+        int sign = -1;
+
+        for (int j = 1; j <= terms; j++) {
+            value += (double)sign * numer / denom;
+            numer *= x[i] * x[i];
+            denom *= (2.0 * j + 2.0) * (2.0 * j + 3.0);
+            sign *= -1;
+        }
+
+        result[i] = value;
+        sprintf(message, "%lf", result[i]);
+        length = strlen(message) + 1;
+        write(fd[2 * i + 1], message, length);
+        exit(child_id);
+    }
+
+    for (int i = 0; i < num_elements; i++) {
+        close(fd[2 * i + 1]);
+    }
+
+    for (int i = 0; i < num_elements; i++) {
+        int status;
+        wait(&status);
+        int child = WEXITSTATUS(status);
+        int n = read(fd[2 * child], line, MAXLINE);
+        result[child] = atof(line);
+    }
 }
 
 int main() {
-    double x[N] = {0, M_PI/6., M_PI/3., 0.134};
-    double result[N];
-    int fd[N][2];
+    double x[N] = {0.0, M_PI / 6, M_PI / 3, 0.134};
+    double res[N];
 
-    for(int i=0; i<N; i++) {
-        if(pipe(fd[i]) == -1) {
-            perror("pipe");
-            exit(1);
-        }
-    }
-    for(int i=0; i<N; i++) {
-        pid_t pid = fork();
-        if(pid < 0) {
-            perror("fork");
-            exit(1);
-        }
-        if(pid == 0) {
-            close(fd[i][0]);
-            double res = sinx_taylor(x[i], TERMS);
-            write(fd[i][1], &res, sizeof(double));
-            close(fd[i][1]);
-            exit(0);
-        } else {
-            close(fd[i][1]);
-        }
-    }
+    sinx_taylor(N, 3, x, res);
 
-    for(int i=0; i<N; i++) {
-        wait(NULL);
-    }
-
-    for(int i=0; i<N; i++) {
-        read(fd[i][0], &result[i], sizeof(double));
-        close(fd[i][0]);
-    }
-    for(int i=0; i<N; i++) {
-        printf("sin(%.3f) by Taylor series = %.6f\n", x[i], result[i]);
-        printf("sin(%.3f) = %.6f\n", x[i], sin(x[i]));
+    for (int i = 0; i < N; i++) {
+        printf("sin(%.3f) by Taylor series = %f\n", x[i], res[i]);
+        printf("sin(%.3f) = %f\n", x[i], sin(x[i]));
     }
 
     return 0;
